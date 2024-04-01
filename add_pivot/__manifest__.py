cd9174c64ac9a2bf8393f984bd0f5bbb3f56f1c9
Derek Loewen <derek.loewen@toews.consulting>
2024-04-01 11:51:07 -0500

    Add add_pivot module

---
+++ add_pivot/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Odoo 16 Example - Adding a Pivot View",
+    "summary": "An example of how to add a pivot view to an existing model.",
+    "author": "Derek Loewen",
+    "website": "https://myles.consulting",
+    "category": "Customizations",
+    "license": "AGPL-3",
+    "version": "0.1",
+    "depends": ["sale"],
+    "auto_install": False,
+    "data": [
+        "views/product_template_pivot.xml"
+    ],
+}
