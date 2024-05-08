82b2e76ec7a670b6d210a1f7ce5e89d2c11bc1c9
Derek Loewen <derek.loewen@toews.consulting>
2024-05-08 17:17:50 -0500

    Patching a static property like "components"

--- js_patching/static/src/js/patches.js
+++ js_patching/static/src/js/patches.js
@@ -2,6 +2,8 @@
 
 import { patch } from "@web/core/utils/patch";
 import { COLORS, getColor } from "@web/views/graph/colors";
+import { ActivityListPopoverItem } from "@mail/core/web/activity_list_popover_item";
+import { FloatField } from '@web/views/fields/float/float_field';
 
 // Since pretty much anything in JavaScript is an object,
 // and the "patch" function can patch objects,
@@ -10,20 +12,26 @@ import { COLORS, getColor } from "@web/views/graph/colors";
 // Patch a constant
 // ------------------------------------------------------------------------
 export const EXTRA_COLORS = ['#333333', '#555555', '#777777'];
-patch(COLORS, "js_patching.patches", [...COLORS, ...EXTRA_COLORS]);
+patch(COLORS, [...COLORS, ...EXTRA_COLORS]);
 
 // Patch a function
 // ------------------------------------------------------------------------
 export function myGetColor(index) {
     return COLORS[index % COLORS.length];
 }
-patch(getColor, "js_patching.patches", myGetColor);
+patch(getColor, myGetColor);
 
 // Patch an OWL component's method
 // ------------------------------------------------------------------------
-patch(GraphView.prototype, "js_patching.patches", {
+patch(GraphView.prototype, {
     setup() {
-        this._super(...arguments);
+        super(...arguments);
         console.log("Hello!")
     }
-})
+});
+
+// Patching a list of components inside an OWL component
+// ------------------------------------------------------------------------
+// The patch function doesn't support static properties inside a class,
+// so we have to use Object.assign instead.
+Object.assign(ActivityListPopoverItem.components, { FloatField });
